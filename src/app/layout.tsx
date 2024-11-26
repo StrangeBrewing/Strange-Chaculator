ad8644fb08e9989d2b48d9c34de711b374693336
StrangeBrewing <101642196+StrangeBrewing@users.noreply.github.com>
2024-11-26 23:04:33 +0100

    Create layout.tsx

---
+++ src/app/layout.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+
+export default function RootLayout({ children }) {
+  return (
+    <html>
+      <head>
+        <title>My App</title>
+      </head>
+      <body>
+        {children}
+      </body>
+    </html>
+  );
+}
