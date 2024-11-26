d087a9df7d2aeb585bdcdabfaec815f24b84b87a
StrangeBrewing <101642196+StrangeBrewing@users.noreply.github.com>
2024-11-26 22:29:07 +0100

    Update page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,9 @@
-import FinancialCalculator from '../components/FinancialCalculator';
+import Chaculator from '../components/Chaculator';
 
 export default function Home() {
   return (
     <main className="min-h-screen p-4">
-      <FinancialCalculator />
+      <Chaculator />
     </main>
   );
 }
