5fe7e4f7a623821d3b322abd83c10867909665da
StrangeBrewing <101642196+StrangeBrewing@users.noreply.github.com>
2024-11-26 22:13:23 +0100

    Create page.tsx

---
+++ src/app/page.tsx
@@ -0,0 +1,9 @@
+import FinancialCalculator from '../components/FinancialCalculator';
+
+export default function Home() {
+  return (
+    <main className="min-h-screen p-4">
+      <FinancialCalculator />
+    </main>
+  );
+}
